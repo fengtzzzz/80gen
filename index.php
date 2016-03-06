931786096e378f98d1a16fd4fefb9e5f275a9385
fengtzzzz <fengtzzzz@gmail.com>
2016-03-06 20:14:25 +0800

    confict

--- index.php
+++ index.php
@@ -2,13 +2,17 @@
 
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../Yii/framework/yii.php';
+//this from server
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
+//this server gogogog
+//not this
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
+//let's roll
 require_once($yii);
 //test
 Yii::createWebApplication($config)->run();
